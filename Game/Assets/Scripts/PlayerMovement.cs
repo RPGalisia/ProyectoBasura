e20f60a2e8fc68df23f4e5cecad2e63d6306e653
Daviddans <109394071+RPGalisia@users.noreply.github.com>
2022-07-19 22:28:11 +0200

    McAnimateUpdate
    Hasta left

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
     private InputAction movement;
     private Rigidbody2D rb;
     private BoxCollider2D box;
+    private Animator anim;
+    private string lastIdle;
     [SerializeField] private int velocity;
     [SerializeField] private int runVelocity;
     private Vector2 move;
@@ -20,6 +22,8 @@ public class PlayerMovement : MonoBehaviour
         playerInputs = new PlayerInputs();
         rb = GetComponent<Rigidbody2D>();
         box = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        lastIdle = "IdleDown";
     }
     private void OnEnable()
     {
@@ -40,13 +44,48 @@ public class PlayerMovement : MonoBehaviour
         move = movement.ReadValue<Vector2>();
         if (move != Vector2.zero)
         {
-            if(run.ReadValue<float>() == 1)
+            if (run.ReadValue<float>() == 1)
                 rb.velocity = move * Time.fixedDeltaTime * runVelocity;
             else
                 rb.velocity = move * Time.fixedDeltaTime * velocity;
-
         }
         else
+        {
             rb.velocity = Vector2.zero;
+        }
+        MoveAnimate();
+    }
+
+    private void MoveAnimate()
+    {
+        if (move == Vector2.zero)
+            anim.Play(lastIdle);
+        else if (move == new Vector2(0, -1))
+            AnimateSelect("Down");
+        else if (move == new Vector2(-1, -1).normalized)
+            AnimateSelect("DownLeft");
+        else if (move == new Vector2(-1, 0))
+            AnimateSelect("Left");
+        else if (move == new Vector2(-1, 1))
+            AnimateSelect("UpLeft");
+        else if (move == new Vector2(0, 1))
+            AnimateSelect("Up");
+    }
+    private void AnimateSelect(string play)
+    {
+        if (move != Vector2.zero)
+        {
+            lastIdle = "Idle" + play;
+            if (run.ReadValue<float>() == 0)
+            {
+                anim.Play("Walk" + play);
+            }
+            else
+                anim.Play("Run" + play);
+        }
+        else
+        {
+            anim.Play(lastIdle);
+        }
     }
 }
